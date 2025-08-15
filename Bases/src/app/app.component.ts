4e4d593608726a27a404fd5e347fab800209b892
Jafeth Jesus <jafethunu@gmail.com>
2025-08-14 18:01:09 -0600

    Creacion pagina goku y manejo de routeLinkActive

--- Bases/src/app/app.component.ts
+++ Bases/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from "./pages/counter/counter.component";
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { DragonballComponent } from './pages/dragonball/dragonball.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent,NavbarComponent],
+  imports: [RouterOutlet, CounterComponent,NavbarComponent,DragonballComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Bases/src/app/app.routes.ts
+++ Bases/src/app/app.routes.ts
@@ -4,8 +4,13 @@ import path from 'path';
 import { HeroPageComponent } from './pages/hero/hero-page/hero-page.component';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './pages/counter/counter.component';
+import { DragonballComponent } from './pages/dragonball/dragonball.component';
 
 export const routes: Routes = [
+  {
+path: 'dragonball',
+component:DragonballComponent
+  },
   {
     path:'',
     component: CounterComponent
