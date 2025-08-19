8d82899d431c8aca1e900219675343841aecb960
Jafeth Jesus <jafethunu@gmail.com>
2025-08-18 23:28:37 -0600

    Manejo de inputs

--- Bases/src/app/pages/dragonball/dragonball.component.ts
+++ Bases/src/app/pages/dragonball/dragonball.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { sign } from 'crypto';
 
+interface Character{
+  id:number;
+  name:string;
+  power:number;
+}
 @Component({
   selector: 'app-dragonball',
   imports: [],
   templateUrl: './dragonball.component.html',
   styleUrl: './dragonball.component.scss'
 })
-export class DragonballComponent {
 
+
+export class DragonballComponent {
+name = signal('Gohan');
+power = signal(100);
+
+character = signal<Character[]>([
+  {id:1, name:'Goku', power:9001},
+  {id:2, name:'Vegeta', power:7500},
+  {id:3, name:'Piccolo', power:4800},
+  {id:4, name:"Yamcha", power:500}
+]);
+
+addCharacter(){
+  const newCharacter: Character ={
+    id: this.character().length,
+    name: this.name(),
+    power: this.power()
+  }
+ //this.character().push(newCharacter);
+ this.character.update((list) => [...list, newCharacter] );
+ this.resetField();
+}
+resetField(){
+  this.name.set(''),
+  this.power.set(0)
+}
 }
